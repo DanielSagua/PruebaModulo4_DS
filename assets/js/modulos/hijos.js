95d80579b34b93fd536afff75b170c5f94226302
Daniel Sagua <danielsagua.n@gmail.com>
2024-05-05 21:23:24 -0400

    uno

---
+++ assets/js/modulos/hijos.js
@@ -0,0 +1,45 @@
+import { Animal } from "./animal.js";
+
+
+
+export class Leon extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    rugir() { 
+        return this.sonido;
+    }
+}
+
+export class Lobo extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    aullar() { }
+}
+
+export class Oso extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    grunir() { }
+}
+
+export class Serpiente extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    sisear() { }
+}
+
+export class Aguila extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    chillar() { }
+}
